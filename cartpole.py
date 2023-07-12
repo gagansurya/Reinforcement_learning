84134a2013365fc8309506e69802309278b3a977
Gagan M Shetty <73263134+gagansurya@users.noreply.github.com>
2023-07-12 22:20:09 +0530

    Add files via upload

---
+++ cartpole.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 12 15:41:16 2023
+
+@author: gaganshetty
+"""
+
+import gymnasium as gym
+
+# from gymnasium.wrappers.monitoring import video_recorder
+#from gymnasium.wrappers.monitoring.video_recorder import video
+
+import time
+env = gym.make("CartPole-v1", render_mode = "human")
+env.reset()
+
+env.render()
+
+#print state space which is continous
+print(env.observation_space)
+
+#print action space
+print(env.action_space)
+
+env.reset()
+#implement with a random policy
+
+n_episodes = 50
+n_timesteps = 50
+
+for i in range(n_episodes):
+    Return = 0
+    for t in range (n_timesteps):
+        env.render()
+        
+        rnd_action = env.action_space.sample()
+        next_state, reward, done,infor,prob = env.step(rnd_action)
+        Return = Return + reward
+        if done:
+            env.reset()
+            break
+        if i%10 == 0:
+            print("Episode : {}, Return : {}".format(i+1, Return))
